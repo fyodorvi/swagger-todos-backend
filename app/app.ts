79a08352721f4d7d901810ff84e84261e4aeb358
fyodor <fedor.jj@gmail.com>
2017-09-30 12:46:54 +1300

    create swagger based api

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
 import * as restify from 'restify';
 import * as corsMiddleware from 'restify-cors-middleware';
+import {ITodoApiController, Todo, TodoApiRouter} from "./generated-interface";
 
 const api = restify.createServer();
 
@@ -10,40 +11,41 @@ api.use(cors.actual);
 api.use(restify.plugins.bodyParser());
 api.use(restify.plugins.queryParser());
 
-interface Todo {
-    id: string;
-    title: string;
-    resolved: boolean;
-}
-
 let todos: Todo[] = [];
 
-api.get('/get-todos', (req: restify.Request, res: restify.Response, next: restify.Next) => {
-    res.send(todos);
-    next();
-});
+class TodoController implements ITodoApiController {
 
-api.post('/add-todo', (req: restify.Request, res: restify.Response, next: restify.Next) => {
-    const todo: Todo = req.body;
-    todo.id = Math.floor(Math.random() * 20).toString();
-    todos = [todo, ...todos];
-    res.send(todos);
-    next();
-});
+    async resolveTodo(parameters: { id: string }): Promise<Array<Todo>> {
+        const todo: Todo = todos.find((todo) => todo.id === parameters.id);
+        todo.resolved = true;
+        return todos;
+    }
 
-api.del('/remove-todo/:id', (req: restify.Request, res: restify.Response, next: restify.Next) => {
-    const index: number = todos.findIndex((todo) => todo.id === req.params.id);
-    todos.splice(index, 1);
-    res.send(todos);
-    next();
-});
+    async removeTodo(parameters: { id: string }): Promise<Array<Todo>> {
+        const index: number = todos.findIndex((todo) => todo.id === parameters.id);
+        todos.splice(index, 1);
+        return todos;
+    }
 
-api.put('/resolve-todo/:id', (req: restify.Request, res: restify.Response, next: restify.Next) => {
-    const todo: Todo = todos.find((todo) => todo.id === req.params.id);
-    todo.resolved = true;
-    res.send(todos);
-    next();
-});
+    async addTodo(parameters: { body?: Todo }): Promise<Array<Todo>> {
+        const todo = new Todo();
+        todo.id = Math.floor(Math.random() * 20).toString();
+        todo.title = parameters.body.title;
+        todo.resolved = false;
+
+        todos = [todo, ...todos];
+        return todos;
+    }
+
+    async getTodos(parameters: {}): Promise<Array<Todo>> {
+        return todos;
+    }
+}
+
+const todoController = new TodoController();
+const todoRouter = new TodoApiRouter(api, todoController);
+
+todoRouter.registerRoutes();
 
 api.listen(3001, function () {
     console.log('sever is listening');
